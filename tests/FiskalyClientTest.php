ed6b0c35e9327a7790f5c339e86c38519fc37719
Mihajlo Nikodijevic <a01646292@unet.univie.ac.at>
2020-05-15 14:17:04 +0200

    - FiskalyClient Tests  - context create and update test  - version test  - configuration test

--- tests/FiskalyClientTest.php
+++ tests/FiskalyClientTest.php
@@ -3,6 +3,10 @@
 namespace FiskalyClient;
 
 use Exception;
+use FiskalyClient\errors\exceptions\FiskalyException;
+use FiskalyClient\responses\ClientConfiguration;
+use FiskalyClient\responses\RequestResponse;
+use FiskalyClient\responses\VersionResponse;
 use PHPUnit\Framework\TestCase;
 
 require_once(__DIR__ . '/../examples/env.php');
@@ -30,5 +34,121 @@ class FiskalyClientTest extends TestCase
         $client = $this->createClient();
         $this->assertNotNull($client);
         $this->assertTrue($client instanceof FiskalyClient);
+    }
+
+    /**
+     * @test
+     */
+    public function testContext()
+    {
+        $client = $this->createClient();
+        $this->assertNotEquals('', $client->getContext());
+    }
+
+    /**
+     * @test
+     */
+    public function testVersion()
+    {
+        try {
+            $client = $this->createClient();
+            $version = $client->getVersion();
+
+            $this->assertNotNull($version);
+            $this->assertTrue($version instanceof VersionResponse);
+            $this->assertNotNull($version->getClientVersion());
+            $this->assertNotNull($version->getSmaersVersion());
+            $this->assertNotNull($version->getClientCommitHash());
+            $this->assertNotNull($version->getClientSourceHash());
+        } catch (Exception $e) {
+            $this->assertTrue(false);
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function testGetConfig()
+    {
+        try {
+            $client = $this->createClient();
+            $config = $client->getConfig();
+
+            $this->assertNotNull($config);
+            $this->assertTrue($config instanceof ClientConfiguration);
+            $this->assertNotNull($config->getClientTimeout());
+            $this->assertNotNull($config->getDebugFile());
+            $this->assertNotNull($config->getDebugLevel());
+            $this->assertNotNull($config->getSmearsTimeout());
+        } catch (Exception $e) {
+            $this->assertTrue(false);
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function testConfigure()
+    {
+        try {
+            $client = $this->createClient();
+
+            try {
+                $config_params = [
+                    'debug_level' => 4,
+                    'debug_file' => __DIR__ . '/../fiskaly.log',
+                    'client_timeout' =>  5000,
+                    'smaers_timeout' =>  2000,
+                ];
+                $config = $client->configure($config_params);
+            } catch (Exception $e) {
+                exit($e);
+            }
+
+            $this->assertNotNull($config);
+            $this->assertTrue($config instanceof ClientConfiguration);
+            $this->assertNotNull($config->getClientTimeout());
+            $this->assertNotNull($config->getDebugFile());
+            $this->assertNotNull($config->getDebugLevel());
+            $this->assertNotNull($config->getSmearsTimeout());
+
+            $this->assertEquals($config_params['debug_level'], $config->getDebugLevel());
+            $this->assertEquals($config_params['debug_file'], $config->getDebugFile());
+            $this->assertEquals($config_params['client_timeout'], $config->getClientTimeout());
+            $this->assertEquals($config_params['smaers_timeout'], $config->getSmearsTimeout());
+        } catch (Exception $e) {
+            $this->assertTrue(false);
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function testRequest()
+    {
+        try {
+            $client = $this->createClient();
+            $response = $client->request(
+                'PUT',
+                '/tss/ecb75169-680f-48d1-93b2-52cc10abb9f/tx/9cbe6566-e24c-42ac-97fe-6a0112fb3c6',
+                ["last_revision" => "0"],
+                ["Content-Type" => "application/json"],
+                'eyJzdGF0ZSI6ICJBQ1RJVkUiLCJjbGllbnRfaWQiOiAiYTYyNzgwYjAtMTFiYi00MThhLTk3MzYtZjQ3Y2E5NzVlNTE1In0='
+            );
+
+            $context = $client->getContext();
+
+            $this->assertNotNull($response);
+            $this->assertTrue($response instanceof RequestResponse);
+            $this->assertNotNull($response->getContext());
+            $this->assertNotNull($response->getResponse());
+
+            // Check if context updated
+            $this->assertNotEquals($context, $response->getContext());
+        } catch (Exception $e) {
+            // echo "Exception: " . $e->getMessage() . "\n";
+            $this->assertTrue($e instanceof FiskalyException);
+            // $this->assertTrue(false);
+        }
     }
 }
