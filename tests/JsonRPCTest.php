58f0ea10e50dc497f56eeca8b58e82f0462bcf0e
Mihajlo Nikodijevic <a01646292@unet.univie.ac.at>
2020-05-18 12:35:38 +0200

    - JsonRPC Library Test - Init without url test - Wrong method name test

---
+++ tests/JsonRPCTest.php
@@ -0,0 +1,43 @@
+<?php
+use Datto\JsonRpc\Http\Client;
+use Datto\JsonRpc\Responses\ErrorResponse;
+use PHPUnit\Framework\TestCase;
+
+require_once(__DIR__ . '/../examples/env.php');
+
+class JsonRPCTest extends TestCase
+{
+
+    /** @var Client|null  */
+    protected $json_rpc = null;
+
+    public function setUp(): void
+    {
+        $this->json_rpc = new Client($_ENV['FISKALY_SERVICE_URL']);
+    }
+
+    public function testInit()
+    {
+        $this->assertNotNull($this->json_rpc);
+    }
+
+    public function testInitWithoutParams()
+    {
+        $this->json_rpc = new Client(null);
+
+        $this->assertNotNull($this->json_rpc);
+        $this->assertTrue($this->json_rpc instanceof Client);
+        $this->assertNotNull($this->json_rpc);
+        $this->assertEquals('close', $this->json_rpc->getHeaders()['Connection']);
+    }
+
+    public function testWrongMethodQuery()
+    {
+        $this->json_rpc->query('wrong-method-name', null, $response)->send();
+
+        $this->assertNotNull($response);
+        $this->assertTrue($response instanceof ErrorResponse);
+        $this->assertEquals(-32601, $response->getCode());
+        $this->assertEquals('Method not found', $response->getMessage());
+    }
+}
