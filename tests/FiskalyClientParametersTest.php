7b64b39d3c3292f5f0c28e04bb908deefca70c30
Mihajlo Nikodijevic <a01646292@unet.univie.ac.at>
2020-05-18 09:15:44 +0200

    - Error (Exception) tests - Client init using context tests

--- tests/FiskalyClientParametersTest.php
+++ tests/FiskalyClientParametersTest.php
@@ -9,11 +9,10 @@ require_once(__DIR__ . '/../examples/env.php');
 
 class FiskalyClientParametersTest extends TestCase
 {
-
     /**
      * @test
      */
-    public function testClientServiceParameter()
+    public function testClientInitServiceParameter()
     {
         try {
             return FiskalyClient::createUsingCredentials(null, null, null, null);
@@ -25,7 +24,7 @@ class FiskalyClientParametersTest extends TestCase
     /**
      * @test
      */
-    public function testClientApiKeyParameter()
+    public function testClientInitApiKeyParameter()
     {
         try {
             return FiskalyClient::createUsingCredentials($_ENV["FISKALY_SERVICE_URL"], null, null, null);
@@ -37,7 +36,7 @@ class FiskalyClientParametersTest extends TestCase
     /**
      * @test
      */
-    public function testClientApiSecretParameter()
+    public function testClientInitApiSecretParameter()
     {
         try {
             return FiskalyClient::createUsingCredentials($_ENV["FISKALY_SERVICE_URL"], $_ENV["FISKALY_API_KEY"], null, null);
@@ -49,12 +48,36 @@ class FiskalyClientParametersTest extends TestCase
     /**
      * @test
      */
-    public function testClientBaseUrlParameter()
+    public function testClientInitBaseUrlParameter()
     {
         try {
             return FiskalyClient::createUsingCredentials($_ENV["FISKALY_SERVICE_URL"], $_ENV["FISKALY_API_KEY"], $_ENV["FISKALY_API_SECRET"], null);
         } catch (Exception $e) {
             $this->assertEquals('base_url must be provided', $e->getMessage());
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function testClientServiceParameterInitUsingContext()
+    {
+        try {
+            return FiskalyClient::createUsingContext(null, null);
+        } catch (Exception $e) {
+            $this->assertEquals('fiskaly_service must be provided', $e->getMessage());
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function testClientContextParameterInitUsingContext()
+    {
+        try {
+            return FiskalyClient::createUsingContext($_ENV["FISKALY_SERVICE_URL"], null);
+        } catch (Exception $e) {
+            $this->assertEquals('context must be provided', $e->getMessage());
         }
     }
 }
